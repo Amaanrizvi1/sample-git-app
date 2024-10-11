f04ea2eba4aaf2b80d9185fefa506943814da9b1
amaan <amaanrizvi.1313@gmail.com>
2024-10-11 12:58:55 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Bleach')
+col1,col2=st.columns(2)
+with col1:
+    st.image('wallpaperflare.com_wallpaper.jpg')
+with col2:
+    st.write("""
+    This is Bleach thousand years blood war arc website """)
+
